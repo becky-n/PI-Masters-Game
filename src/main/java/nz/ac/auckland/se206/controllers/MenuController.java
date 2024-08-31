fe8c594fdcee08997707994ec820ca3e2742b9d4
ayepricots <160553101+ayepricots@users.noreply.github.com>
2024-09-01 04:22:56 +1200

    added wedding march background music to menu screen

--- src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
@@ -3,12 +3,22 @@ package nz.ac.auckland.se206.controllers;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.media.AudioClip;
 import nz.ac.auckland.apiproxy.exceptions.ApiProxyException;
 
+/**
+ * Controller class for the menu view. Handles user interactions within the menu where the user can
+ * start the game.
+ */
 public class MenuController {
+  private AudioClip backgroundMusic;
 
+  /** Initialises the menu scene. */
   @FXML
-  public void initialize() {}
+  public void initialize() {
+    backgroundMusic = new AudioClip(getClass().getResource("/sounds/wedding_march.mp3").toString());
+    backgroundMusic.play();
+  }
 
   @FXML
   private void onStart(ActionEvent event) throws ApiProxyException, IOException {}
