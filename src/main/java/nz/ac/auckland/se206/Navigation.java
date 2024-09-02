87db1ede1221f883b3f1cdf67e98f2a6d77be756
Becky <beckyngan28@gmail.com>
2024-09-02 17:33:04 +1200

    adding sound effects and padding

--- src/main/java/nz/ac/auckland/se206/Navigation.java
+++ src/main/java/nz/ac/auckland/se206/Navigation.java
@@ -2,15 +2,19 @@ package nz.ac.auckland.se206;
 
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.media.AudioClip;
+
 import java.io.IOException; 
 
 
 public class Navigation {
+  private AudioClip buttonClickSound;
 
   public Navigation() {
   }
 
   public void setMenu(MenuButton menuButton) {
+    buttonClickSound = new AudioClip(getClass().getResource("/sounds/click.mp3").toString());
     // Set the menu
     menuButton.getItems().clear();
 
@@ -23,11 +27,12 @@ public class Navigation {
 
 
     // Set the action
-    gerald.setOnAction(e -> changeScene("aisle"));
-    jesin.setOnAction(e -> changeScene("ballroom"));
-    crimeScene.setOnAction(e -> changeScene("crime"));
-    andrea.setOnAction(e -> changeScene("lobby"));
+    gerald.setOnAction(e -> { changeScene("aisle"); buttonClickSound.play(); });
+    jesin.setOnAction(e -> {changeScene("ballroom"); buttonClickSound.play(); });
+    crimeScene.setOnAction(e -> {changeScene("crime"); buttonClickSound.play(); });
+    andrea.setOnAction(e -> {changeScene("lobby");buttonClickSound.play(); });
 
+    menuButton.setOnShowing(e -> buttonClickSound.play());
   }
 
   private void changeScene(String scene) {
