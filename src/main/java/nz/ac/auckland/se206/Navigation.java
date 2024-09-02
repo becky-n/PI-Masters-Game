d8c5ec85a7fd0bca268622d0ea233cd4f8f4bca2
Becky <beckyngan28@gmail.com>
2024-09-02 16:02:17 +1200

    adding a basic drop down menu for room changes

---
+++ src/main/java/nz/ac/auckland/se206/Navigation.java
@@ -0,0 +1,41 @@
+package nz.ac.auckland.se206;
+
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import nz.ac.auckland.se206.App;
+import java.io.IOException; 
+
+
+public class Navigation {
+
+  public Navigation() {
+  }
+
+  public void setMenu(MenuButton menuButton) {
+    // Set the menu
+    menuButton.getItems().clear();
+
+    MenuItem gerald = new MenuItem("Gerald");
+    MenuItem jesin = new MenuItem("Jesin");
+    MenuItem crimeScene = new MenuItem("Crime Scene");
+    MenuItem andrea = new MenuItem("Andrea");
+
+    menuButton.getItems().addAll(gerald, jesin, crimeScene, andrea);
+
+    // Set the action
+    gerald.setOnAction(e -> changeScene("aisle"));
+    jesin.setOnAction(e -> changeScene("ballroom"));
+    crimeScene.setOnAction(e -> changeScene("crime"));
+    andrea.setOnAction(e -> changeScene("suite"));
+
+  }
+
+  private void changeScene(String scene) {
+    try {
+      App.setRoot(scene);
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+}
