541924dc9df4181190ac6593d9e41fcc07ec850d
ayepricots <160553101+ayepricots@users.noreply.github.com>
2024-09-13 22:35:16 +1200

    replaced fabric images, added hover effects and more sound effects

--- src/main/java/nz/ac/auckland/se206/controllers/WindowController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/WindowController.java
@@ -25,17 +25,28 @@ import nz.ac.auckland.se206.TimerManager;
 
 public class WindowController {
   private AudioClip buttonClickSound;
+  private AudioClip twinkleSound;
 
-  @FXML private MenuButton menuButton;
-  @FXML private Label timerLabel;
-  @FXML private Pane clueMenu;
-  @FXML private ImageView glass1;
-  @FXML private ImageView glass2;
-  @FXML private ImageView glass3;
-  @FXML private ImageView glass4;
-  @FXML private ImageView glass5;
-  @FXML private Label infoLabel;
-  @FXML private ImageView fabric;
+  @FXML
+  private MenuButton menuButton;
+  @FXML
+  private Label timerLabel;
+  @FXML
+  private Pane clueMenu;
+  @FXML
+  private ImageView glass1;
+  @FXML
+  private ImageView glass2;
+  @FXML
+  private ImageView glass3;
+  @FXML
+  private ImageView glass4;
+  @FXML
+  private ImageView glass5;
+  @FXML
+  private Label infoLabel;
+  @FXML
+  private ImageView fabric;
 
   private static GameStateContext context = new GameStateContext();
 
@@ -44,7 +55,6 @@ public class WindowController {
 
     // initialize fabric image
     fabric.setImage(new Image("/images/fabric1.png"));
-
 
     // Make the glass images draggable
     DraggableMaker dm = new DraggableMaker();
@@ -62,6 +72,7 @@ public class WindowController {
     }
 
     buttonClickSound = new AudioClip(getClass().getResource("/sounds/click.mp3").toString());
+    twinkleSound = new AudioClip(getClass().getResource("/sounds/twinkle.mp3").toString());
 
     // Initialize the controller
     Navigation nav = new Navigation();
@@ -74,10 +85,9 @@ public class WindowController {
         .textProperty()
         .bind(
             Bindings.createStringBinding(
-                () ->
-                    String.format(
-                        "%02d:%02d",
-                        timerManager.getTimeRemaining() / 60, timerManager.getTimeRemaining() % 60),
+                () -> String.format(
+                    "%02d:%02d",
+                    timerManager.getTimeRemaining() / 60, timerManager.getTimeRemaining() % 60),
                 timerManager.timeRemainingProperty()));
 
     // animate the text in the info label
@@ -113,15 +123,37 @@ public class WindowController {
     buttonClickSound.play();
     // if image is fabric1, change to fabric2
     if (fabric.getImage().getUrl().contains("fabric1")) {
-      System.out.println("changing to fabric2");
       fabric.setImage(new Image("/images/fabric2.png"));
     } else if (fabric.getImage().getUrl().contains("fabric2")) {
-      System.out.println("changing to fabric3");
       fabric.setImage(new Image("/images/fabric3.png"));
     } else if (fabric.getImage().getUrl().contains("fabric3")) {
-      System.out.println("changing to fabric4");
       fabric.setImage(new Image("/images/fabric4.png"));
+      twinkleSound.play();
       animateText("A torn piece of black fabric… it seems familiar, but where have I seen it before?");
+    }
+
+    onHoverFabric();
+  }
+
+  @FXML
+  public void onHoverFabric() {
+    if (fabric.getImage().getUrl().contains("fabric1")) {
+      fabric.setImage(new Image("/images/fabric1hover.png"));
+    } else if (fabric.getImage().getUrl().contains("fabric2")) {
+      fabric.setImage(new Image("/images/fabric2hover.png"));
+    } else if (fabric.getImage().getUrl().contains("fabric3")) {
+      fabric.setImage(new Image("/images/fabric3hover.png"));
+    }
+  }
+
+  @FXML
+  public void offHoverFabric() {
+    if (fabric.getImage().getUrl().contains("fabric1")) {
+      fabric.setImage(new Image("/images/fabric1.png"));
+    } else if (fabric.getImage().getUrl().contains("fabric2")) {
+      fabric.setImage(new Image("/images/fabric2.png"));
+    } else if (fabric.getImage().getUrl().contains("fabric3")) {
+      fabric.setImage(new Image("/images/fabric3.png"));
     }
   }
 
@@ -135,17 +167,16 @@ public class WindowController {
   private void animateText(String str) {
     final IntegerProperty i = new SimpleIntegerProperty(0);
     Timeline timeline = new Timeline();
-    KeyFrame keyFrame =
-        new KeyFrame(
-            Duration.seconds(0.015), // Adjusted for smoother animation
-            event -> {
-              if (i.get() > str.length()) {
-                timeline.stop();
-              } else {
-                infoLabel.setText(str.substring(0, i.get()));
-                i.set(i.get() + 1);
-              }
-            });
+    KeyFrame keyFrame = new KeyFrame(
+        Duration.seconds(0.015), // Adjusted for smoother animation
+        event -> {
+          if (i.get() > str.length()) {
+            timeline.stop();
+          } else {
+            infoLabel.setText(str.substring(0, i.get()));
+            i.set(i.get() + 1);
+          }
+        });
     timeline.getKeyFrames().add(keyFrame);
     timeline.setCycleCount(Animation.INDEFINITE);
     timeline.play();
